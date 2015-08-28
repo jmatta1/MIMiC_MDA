074b29ca09304a1efeacb3b33d171823269c0185
James Matta <jamesmatta@gmail.com>
2015-08-27 22:07:46 -0500

    a few tweaks for better timing output, near as I can tell, the make struct part of c testing is, invisibly, paying the price both of loading the library and making the struct

--- C/ChiSqTester.c
+++ C/ChiSqTester.c
@@ -6,14 +6,14 @@ int main(int argc, char* argv[])
 {
     clock_t t1, t2;
     t1=clock();
-    float diff;
+    long long int diff;
     printf("starting C testing\n");
     //test the allocate function
     t1 = clock();
     MdaData* dataPtr = makeMdaStruct(10, 3);
     t2 = clock();
-    diff = (((float)(t2-t1))/CLOCKS_PER_SEC);
-    printf("make struct, took: %f\n", diff);
+    diff = (((t2-t1)*1000000)/CLOCKS_PER_SEC);
+    printf("make struct, took: %d microseconds\n", diff);
     
     //make some data
     float testArr[10] = {0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9};
@@ -22,15 +22,15 @@ int main(int argc, char* argv[])
     t1 = clock();
     setData(dataPtr, testArr);
     t2 = clock();
-    diff = (((float)(t2-t1))/CLOCKS_PER_SEC);
-    printf("set data, took: %f\n", diff);
+    diff = (((t2-t1)*1000000)/CLOCKS_PER_SEC);
+    printf("set data, took: %d microseconds\n", diff);
     
     //test the deallocate function
     t1 = clock();
     freeMdaStruct(dataPtr);
     t2 = clock();
-    diff = (((float)(t2-t1))/CLOCKS_PER_SEC);
-    printf("del struct, took: %f\n", diff);
+    diff = (((t2-t1)*1000000)/CLOCKS_PER_SEC);
+    printf("free struct, took: %d microseconds\n", diff);
     
     //exit
     return 0;
--- C/so_py_tester.py
+++ C/so_py_tester.py
@@ -3,28 +3,30 @@ import numpy as np
 import time
 
 def main():
-    print "starting python testing"
     # load the library
+    t1 = time.time()
     cs_lib = cdll.LoadLibrary("./libChiSq.so")
+    t2 = time.time()
+    print "load library, took", int((t2-t1)*1000000), "microseconds"
     # tell python the function will return a void pointer
     cs_lib.makeMdaStruct.restype = c_void_p
     # make the strcuture to hold the MdaData with 50 data points and 8 L dists
     t1 = time.time()
     mdaData = cs_lib.makeMdaStruct(10,3)
     t2 = time.time()
-    print "make struct, took", (t2-t1), "seconds"
+    print "make struct, took", int((t2-t1)*1000000), "microseconds"
     # make an array with the test data
     divDat = np.array([0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9], np.float32)
     #run the function to load the array into the struct
     t1 = time.time()
     cs_lib.setData(mdaData, divDat.ctypes.data)
     t2 = time.time()
-    print "set data, took", (t2-t1), "seconds"
+    print "set data, took", int((t2-t1)*1000000), "microseconds"
     #free the structure
     t1 = time.time()
     cs_lib.freeMdaStruct(mdaData)
     t2 = time.time()
-    print "free struct, took", (t2-t1), "seconds"
+    print "free struct, took", int((t2-t1)*1000000), "microseconds"
 
 
 if __name__ == "__main__":
