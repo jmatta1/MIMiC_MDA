28c357d409bee4573e5e1cdc17088f7b0cae32f3
James Matta <jamesmatta@gmail.com>
2015-08-27 21:11:28 -0500

    set the python script to delete the memory it allocated

--- C/so_py_tester.py
+++ C/so_py_tester.py
@@ -1,11 +1,23 @@
 from ctypes import *
+import numpy as np
 
-chi_sq_lib = cdll.LoadLibrary("./libChiSq.so")
+def main():
+    # load the library
+    chi_sq_lib = cdll.LoadLibrary("./libChiSq.so")
+    # extract the function to generate the struct
+    makeMdaStruct = chi_sq_lib.makeMdaStruct
+    # extract the function to free the struct
+    freeMdaStruct = chi_sq_lib.freeMdaStruct
+    # tell python the function will return a void pointer
+    makeMdaStruct.restype = c_void_p
+    # make the strcuture to hold the MdaData with 50 data points and 8 L dists
+    mdaData = makeMdaStruct(50,8)
+    # print the pointer to the structure
+    print hex(mdaData)
+    #free the structure
+    freeMdaStruct(mdaData)
 
-makeMdaStruct = chi_sq_lib.makeMdaStruct
 
-makeMdaStruct.restype = c_void_p
 
-mdaData = makeMdaStruct(50,8)
-
-print mdaData
+if __name__ == "__main__":
+    main()
