c455d61cab5a3f554425e866888ab307e11394fa
James Matta <jamesmatta@gmail.com>
2015-08-30 19:50:04 -0400

    made skeleton contents for the files

---
+++ mda.py
@@ -0,0 +1,10 @@
+import sys
+import argparse
+from mda_config import config
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ mda_config.py
@@ -0,0 +1,12 @@
+config={}
+config["Input File Name"]=./test.csv
+
+# Sets if IVGDR subtraction is carried carried out.
+config["Subtract IVGDR"]=True
+# Integral across all ex energy of the IVGDR lorientzian, doesn't matter if
+# subtraction is off
+config["IVGDR CS Integral"]=1200.0
+# Height of the IVGDR lorentzian in millibarns
+config["IVGDR Height"]=200.0
+config["IVGDR Center"]=12.0
+config["IVGDR Width"]=3.0
