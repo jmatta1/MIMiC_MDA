3b3f010e92515f3b082a9937fc3a64c94c9b3e86
James Matta <jamesmatta@gmail.com>
2015-08-30 20:07:41 -0400

    added some more options to the configuration file

--- mda_config.py
+++ mda_config.py
@@ -1,12 +1,19 @@
 config={}
-config["Input File Name"]=./test.csv
+
+# path to the file containing the experimental data
+config["Input File Path"] = ./test.csv
+# path to the shared library that contains the compiled routines for fast calc
+# of chi^2, log likelihood, and prior distributions
+config["Shared Lib Path"] = "./C/libChiSq.so"
 
 # Sets if IVGDR subtraction is carried carried out.
-config["Subtract IVGDR"]=True
+config["Subtract IVGDR"] = True
 # Integral across all ex energy of the IVGDR lorientzian, doesn't matter if
 # subtraction is off
-config["IVGDR CS Integral"]=1200.0
+config["IVGDR CS Integral"] = 1200.0
 # Height of the IVGDR lorentzian in millibarns
-config["IVGDR Height"]=200.0
-config["IVGDR Center"]=12.0
-config["IVGDR Width"]=3.0
+config["IVGDR Height"] = 200.0
+# centroid energy (in MeV) of the IVGDR lorentzian
+config["IVGDR Center"] = 12.0
+# width of the IVGDR lorentzian in MeV
+config["IVGDR Width"] = 3.0
