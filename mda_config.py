56b762e673965091bc9bfc3585281353bafef685
James Matta <jamesmatta@gmail.com>
2015-08-31 16:15:38 -0400

    Added more comments to the configuration parameters and added a few more parameters

--- mda_config.py
+++ mda_config.py
@@ -1,19 +1,53 @@
-config={}
+"""This file contains the configuration dictionary. This dictionary stores all
+the parameters needed for the code to run using names that are human readable
+and is intended to be user editable"""
 
-# path to the file containing the experimental data
-config["Input File Path"] = ./test.csv
+# This is the dictionary that stores configuration information
+CONFIGURATION = {}
+
 # path to the shared library that contains the compiled routines for fast calc
 # of chi^2, log likelihood, and prior distributions
-config["Shared Lib Path"] = "./C/libChiSq.so"
+CONFIGURATION["Shared Lib Path"] = "./C/libChiSq.so"
 
+# Information about inputs to the program
+# path to the file containing the experimental data
+CONFIGURATION["Input File Path"] = "./test.csv"
+# path to the directory that contains the dwba GR distributions for 100% EWSR
+CONFIGURATION["Distribution Directory"] = "./dist/"
+
+
+# Information about the target nucleus
+# A of the target nucleus
+CONFIGURATION["Target A"] = 58
 # Sets if IVGDR subtraction is carried carried out.
-config["Subtract IVGDR"] = True
+CONFIGURATION["Subtract IVGDR"] = True
 # Integral across all ex energy of the IVGDR lorientzian, doesn't matter if
 # subtraction is off
-config["IVGDR CS Integral"] = 1200.0
+CONFIGURATION["IVGDR CS Integral"] = 1200.0
 # Height of the IVGDR lorentzian in millibarns
-config["IVGDR Height"] = 200.0
+CONFIGURATION["IVGDR Height"] = 200.0
 # centroid energy (in MeV) of the IVGDR lorentzian
-config["IVGDR Center"] = 12.0
+CONFIGURATION["IVGDR Center"] = 12.0
 # width of the IVGDR lorentzian in MeV
-config["IVGDR Width"] = 3.0
+CONFIGURATION["IVGDR Width"] = 3.0
+
+# Limits on and paramters of the fit
+# Maximum L value to fit with
+CONFIGURATION["Maximum L"] = 7
+# Maximum Angle (in degrees) to fit with
+CONFIGURATION["Max Theta"] = 10.0
+# Confidence interval of the error bars
+# ((CONFIGURATION["Sample Points"] - 50) * CONFIGURATION["Number of Walkers"])
+# must exceed, by a large margin, the following value:
+# 1 / ((1 - CONFIGURATION["Confidence Interval"]) / 2)
+# 1 sigma -> 0.682689492
+# 2 sigma -> 0.954499736
+# 3 sigma -> 0.997300204
+# 4 sigma -> 0.99993666
+CONFIGURATION["Confidence Interval"] = 0.682689492
+# holds the number of points to sample for each walker
+CONFIGURATION["Sample Points"] = 500
+# holds the number of walks to run
+CONFIGURATION["Number of Walkers"] = 1000
+# holds the number of concurrent threads
+CONFIGURATION["Number of Threads"] = 4
