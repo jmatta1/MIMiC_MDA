5ab1c35a980c089964fbd720d99683b087aa273b
James Matta <jamesmatta@gmail.com>
2015-09-02 13:33:29 -0400

    Added testing for the log liklihood function, also made a python calculator of the log liklihood just to see the time difference between library call and straight python

--- C/so_py_tester.py
+++ C/so_py_tester.py
@@ -13,6 +13,7 @@ def main():
     # tell python the function will return a void pointer
     cs_lib.makeMdaStruct.restype = c_void_p
     cs_lib.calculateChi.restype = c_float
+    cs_lib.calculateLnLiklihood.restype = c_float
     
     # make the strcuture to hold the MdaData with 50 data points and 8 L dists
     t1 = time.time()
@@ -23,13 +24,13 @@ def main():
     # make an array with the test data
     divDat = np.array([0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9], np.float32)
     
-    #run the function to load the array into the struct
+    # run the function to load the array into the struct
     t1 = time.time()
     cs_lib.setMdaData(mdaData, divDat.ctypes.data)
     t2 = time.time()
     print "set data, took", int((t2-t1)*1000000), "microseconds"
     
-    #run the function to load the array into the struct
+    # run the function to load the array into the struct
     t1 = time.time()
     cs_lib.setMdaDist(mdaData, 0, divDat.ctypes.data)
     cs_lib.setMdaDist(mdaData, 1, divDat.ctypes.data)
@@ -40,20 +41,44 @@ def main():
     # make an array with the test data
     params = np.array([0.2, 0.2, 0.2], np.float32)
     
-    #run the function to calculate a chi^2
+    # run the function to calculate a chi^2
     t1 = time.time()
     chi = cs_lib.calculateChi(mdaData, params.ctypes.data)
     for _ in range(30):
         cs_lib.calculateChi(mdaData, params.ctypes.data)
     t2 = time.time()
-    print "set data, took", int((t2-t1)*1000000), "microseconds"
+    print "calc chi, took", int((t2-t1)*1000000), "microseconds"
     print "  the chi was:",chi
     
-    #free the structure
+    # run the function to calculate a chi^2
+    t1 = time.time()
+    chi = cs_lib.calculateLnLiklihood(mdaData, params.ctypes.data)
+    for _ in range(30):
+        cs_lib.calculateLnLiklihood(mdaData, params.ctypes.data)
+    t2 = time.time()
+    print "calc log liklihood, took", int((t2-t1)*1000000), "microseconds"
+    print "  the chi was:",chi
+    
+    # free the structure
     t1 = time.time()
     cs_lib.freeMdaStruct(mdaData)
     t2 = time.time()
     print "free struct, took", int((t2-t1)*1000000), "microseconds"
+    
+    # as a test, calculate the chi^2 in the pythonic way
+    t1 = time.time()
+    residuals = divDat
+    for param in params:
+        residuals -= param*divDat
+    lnlik = np.sum(np.power(residuals,2))/(-2.0)
+    for _ in range(30):
+        residuals = divDat
+        for param in params:
+            residuals -= param*divDat
+        temp = np.sum(np.power(residuals,2))/(-2.0)
+    t2 = time.time()
+    print "python calc log liklihood, took", int((t2-t1)*1000000), "microseconds"
+    print "  the chi was:",lnlik
 
 
 if __name__ == "__main__":
