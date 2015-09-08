4aa4e0fb0d72064e5fe8cdafa54c16cc9c8f5ce7
James Matta <jamesmatta@gmail.com>
2015-09-07 21:06:04 -0400

    added a few more options

--- mda_config.py
+++ mda_config.py
@@ -8,13 +8,13 @@ CONFIG = {}
 
 # path to the shared library that contains the compiled routines for fast calc
 # of chi^2, log likelihood, and prior distributions
-CONFIG["Shared Lib Path"] = "./C/libChiSq.so"
+CONFIG["Shared Lib Path"] = "./C/libChiSq.so"  # complete
 
 # Information about inputs to the program
 # path to the file containing the experimental data
-CONFIG["Input File Path"] = "./58Ni_cefatz_en_row_decomp.csv"  # used
+CONFIG["Input File Path"] = "./58Ni_cefatz_en_row_decomp.csv"  # complete
 # path to the directory that contains the dwba GR distributions for 100% EWSR
-CONFIG["Distribution Directory"] = "./dist/"
+CONFIG["Distribution Directory"] = "./dist/"  # complete
 
 # Information about outputs from the program
 # file to hold the fitted parameters
@@ -31,57 +31,63 @@ CONFIG["Parameter Plots Directory"] = "./fits/param_plots/"
 
 # Information about the target nucleus
 # A of the target nucleus
-CONFIG["Target A"] = 58
+CONFIG["Target A"] = 58  # complete
 # Sets if IVGDR subtraction is carried carried out.
-CONFIG["Subtract IVGDR"] = True
+CONFIG["Subtract IVGDR"] = True  # complete
 # Integral across all ex energy of the IVGDR lorientzian, doesn't matter if
 # subtraction is off
-CONFIG["IVGDR CS Integral"] = 294.0  # used
+CONFIG["IVGDR CS Integral"] = 294.0  # complete
 # Height of the IVGDR lorentzian in millibarns
-CONFIG["IVGDR Height"] = 23.6484  # used
+CONFIG["IVGDR Height"] = 23.6484  # complete
 # centroid energy (in MeV) of the IVGDR lorentzian
-CONFIG["IVGDR Center"] = 19.0984  # used
+CONFIG["IVGDR Center"] = 19.0984  # complete
 # width of the IVGDR lorentzian in MeV
-CONFIG["IVGDR Width"] = 7.91453  # used
+CONFIG["IVGDR Width"] = 7.91453  # complete
 
 # Information setting what energies and angles are fit
 # Maximum Angle (in degrees) to fit with
-CONFIG["Max Theta"] = 10.0  # used
+CONFIG["Max Theta"] = 10.0  # complete
 # Energy such that all fitted energies are >= to it
-CONFIG["Start Energy"] = 11.4  # used
+CONFIG["Start Energy"] = 11.4  # complete
 # Energy such that all fitted energies are <= to it
-CONFIG["Final Energy"] = 11.6  # used
+CONFIG["Final Energy"] = 11.6  # complete
 
 # Limits on and paramters of the fit
 # Maximum L value to fit with
-CONFIG["Maximum L"] = 7
+CONFIG["Maximum L"] = 7  # incomplete
 # List of sum rule fractions (on the interval [0, 1])
-CONFIG["EWSR Fractions"] = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-# Lists of starting points for each a_L in the initial fits
-# (max a_L = 1.0/(EWSR Fraction_L)
-# ... This continues for as many Ls as you have
-# for example: CONFIG["Start Pts a23"] = [0.5]
-CONFIG["Start Pts a0"] = [0.1, 0.5, 0.9]
-CONFIG["Start Pts a1"] = [0.1, 0.5, 0.9]
-CONFIG["Start Pts a2"] = [0.1, 0.5, 0.9]
-CONFIG["Start Pts a3"] = [0.1, 0.5, 0.9]
-CONFIG["Start Pts a4"] = [0.2, 0.8]
-CONFIG["Start Pts a5"] = [0.2, 0.8]
-CONFIG["Start Pts a6"] = [0.5]
-CONFIG["Start Pts a7"] = [0.5]
+CONFIG["EWSR Fractions"] = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]  # complete
 
 # Confidence interval of the error bars
-# ((CONFIG["Sample Points"] - 50) * CONFIG["Number of Walkers"])
-# must exceed, by a large margin, the following value:
-# 1 / ((1 - CONFIG["Confidence Interval"]) / 2)
+# ((CONFIG["Sample Points"] - CONFIG["Burn-in Points"]) 
+#     CONFIG["Number of Walkers"]) must exceed, by a fair margin, the following
+# value: 1 / ((1 - CONFIG["Confidence Interval"]) / 2)
 # 1 sigma -> 0.682689492
 # 2 sigma -> 0.954499736
 # 3 sigma -> 0.997300204
 # 4 sigma -> 0.99993666
-CONFIG["Confidence Interval"] = 0.682689492
+CONFIG["Confidence Interval"] = 0.682689492  # unused
 # holds the number of points to sample for each walker
-CONFIG["Sample Points"] = 500
+CONFIG["Sample Points"] = 8050  # complete
 # holds the number of walks to run
-CONFIG["Number of Walkers"] = 1000
+CONFIG["Number of Walkers"] = 2000  # complete
 # holds the number of concurrent threads
-CONFIG["Number of Threads"] = 4
+CONFIG["Number of Threads"] = 4  # complete
+# Lists of starting points for each a_L in the initial fits
+# (max a_L = 1.0/(EWSR Fraction_L)
+# ... This continues for as many Ls as you have
+# for example: CONFIG["Start Pts a23"] = [0.5]
+CONFIG["Start Pts a0"] = [0.1, 0.5, 0.9]  # complete
+CONFIG["Start Pts a1"] = [0.1, 0.5, 0.9]  # complete
+CONFIG["Start Pts a2"] = [0.1, 0.5, 0.9]  # complete
+CONFIG["Start Pts a3"] = [0.1, 0.5, 0.9]  # complete
+CONFIG["Start Pts a4"] = [0.2, 0.8]  # complete
+CONFIG["Start Pts a5"] = [0.2, 0.8]  # complete
+CONFIG["Start Pts a6"] = [0.5]  # complete
+CONFIG["Start Pts a7"] = [0.5]  # complete
+# Number of refined points to use to generate initial positions for walkers
+CONFIG["Number Walker Generators"] = 20  # complete
+# Spread of the initial sampling positions for walkers
+CONFIG["Sample Spread"] = 0.05  # complete
+# Number of points at the beginning of the Markov Chain to discard as 'burn in'
+CONFIG["Burn-in Points"] = 50  # incomplete
