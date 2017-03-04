e0bc2cce231ac88715c6605d9e1da886a01ee118
James Till Matta <jamesmatta@gmail.com>
2017-03-04 15:57:57 -0500

    Added some improved documentation and extra params
    These extra parameters include the directory for storage of the parameter
    time series plots, and parameters that control the centroid and spread of
    the offset to be applied to initial starting parameters that are passed to
    the walkers from bfgs fitting
    
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- mda_config0.py
+++ mda_config0.py
@@ -8,15 +8,15 @@ CONFIG = {}
 
 # path to the shared library that contains the compiled routines for fast calc
 # of chi^2, log likelihood, and prior distributions
-CONFIG["Shared Lib Path"] = "./C/libChiSq.so"  # complete
+CONFIG["Shared Lib Path"] = "./C/libChiSq.so"
 
 # Information about inputs to the program
 # path to the file containing the experimental data
-CONFIG["Input File Path"] = "./58Ni_cefatz_en_row_decomp.csv"  # complete
+CONFIG["Input File Path"] = "./58Ni_cefatz_en_row_decomp.csv"
 # path to the directory that contains the dwba GR distributions
 # the distributions, regardless of sum rule fraction, should all start at 0 deg
 # and have identical sampling angles and end points
-CONFIG["Distribution Directory"] = "./dist/"  # complete
+CONFIG["Distribution Directory"] = "./dist/"
 
 # Information about outputs from the program
 # file to hold the fitted parameters
@@ -32,9 +32,11 @@ CONFIG["Prob Plots Directory"] = "./fits0/prob_plots/"
 CONFIG["Fit Plots Directory"] = "./fits0/fit_plots/"
 # directory to store sampled Markov Chains (in the compressed numpy format)
 # this is used by teh seperate make triangles code as a data source
-CONFIG["Chain Directory"] = "./fits0/chains/"  # complete
+CONFIG["Chain Directory"] = "./fits0/chains/"
 # directory to store parameter plots
 CONFIG["Parameter Plots Directory"] = "./fits0/param_plots/"
+# directory to store the autocorrellation plot sub directories
+CONFIG["Time Series Directory"] = "./fits0/walker_plots/"  # incomplete
 # format for plot outputs. options are:
 # "svg" - scalable vector graphics
 # "svgz" - scalable vector graphics (compressed)
@@ -60,36 +62,36 @@ CONFIG["Fit Plot L Limit"] = 3
 # Toggle the output of sample chains, if you disable the output of Corner plots
 # this needs to be on if you later want to make the corner plots in the
 # seperate script if you want to analyze the samples in other ways
-CONFIG["Save Chain Data"] = True
+CONFIG["Save Chain Data"] = False 
 
 # Information about the target nucleus
 # A of the target nucleus
-CONFIG["Target A"] = 58  # complete
+CONFIG["Target A"] = 58
 # Sets if IVGDR subtraction is carried carried out.
-CONFIG["Subtract IVGDR"] = True  # complete
+CONFIG["Subtract IVGDR"] = True
 # Integral across all ex energy of the IVGDR lorientzian, doesn't matter if
 # subtraction is off
-CONFIG["IVGDR CS Integral"] = 294.0  # complete
+CONFIG["IVGDR CS Integral"] = 294.0
 # Height of the IVGDR lorentzian in millibarns
-CONFIG["IVGDR Height"] = 23.6484  # complete
+CONFIG["IVGDR Height"] = 23.6484
 # centroid energy (in MeV) of the IVGDR lorentzian
-CONFIG["IVGDR Center"] = 19.0984  # complete
+CONFIG["IVGDR Center"] = 19.0984
 # width of the IVGDR lorentzian in MeV
-CONFIG["IVGDR Width"] = 7.91453  # complete
+CONFIG["IVGDR Width"] = 7.91453
 
 # Information setting what energies and angles are fit
 # Maximum Angle (in degrees) to fit with
-CONFIG["Max Theta"] = 10.0  # complete
+CONFIG["Max Theta"] = 10.0
 # Energy such that all fitted energies are >= to it
-CONFIG["Start Energy"] = 11.4  # complete
+CONFIG["Start Energy"] = 11.4
 # Energy such that all fitted energies are <= to it
-CONFIG["Final Energy"] = 36.6  # complete
+CONFIG["Final Energy"] = 36.6
 
 # Limits on and paramters of the fit
 # Maximum L value to fit with
-CONFIG["Maximum L"] = 7  # incomplete
+CONFIG["Maximum L"] = 7
 # List of sum rule fractions (on the interval [0, 1])
-CONFIG["EWSR Fractions"] = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]  # complete
+CONFIG["EWSR Fractions"] = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
 
 # Confidence interval of the error bars
 # number of sample points (see notes below) must exceed, by a fair margin,
@@ -98,13 +100,15 @@ CONFIG["EWSR Fractions"] = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]  # complete
 # 2 sigma -> 0.954499736
 # 3 sigma -> 0.997300204
 # 4 sigma -> 0.99993666
-CONFIG["Confidence Interval"] = 0.682689492  # complete
-# holds the number of points to sample for each walker
-CONFIG["Sample Points"] = 4050  # complete
+CONFIG["Confidence Interval"] = 0.682689492
+# holds the number of points to sample for each walker, should be about 10
+# autocorrellation times, 1 auto correllation time is the time it takes for the
+# walkers for all parameters to drift into their designated sampling region
+CONFIG["Sample Points"] = 4050
 # holds the number of walks to run
-CONFIG["Number of Walkers"] = 2000  # complete
+CONFIG["Number of Walkers"] = 2000
 # holds the number of concurrent threads
-CONFIG["Number of Threads"] = 4  # complete
+CONFIG["Number of Threads"] = 4
 # holds the number of bins to use when determining the peak of the probability
 # density distribution
 CONFIG["Num Bins"] = 1000
@@ -115,22 +119,27 @@ CONFIG["Float Epsilon"] = 1.0e-7
 # (max a_L = 1.0/(EWSR Fraction_L)
 # ... This continues for as many Ls as you have
 # for example: CONFIG["Start Pts a23"] = [0.5]
-CONFIG["Start Pts a0"] = [0.0, 0.05, 0.10, 0.15, 0.2]  # complete
-CONFIG["Start Pts a1"] = [0.0, 0.05, 0.10, 0.15, 0.2]  # complete
-CONFIG["Start Pts a2"] = [0.0, 0.05, 0.10, 0.15, 0.2]  # complete
-CONFIG["Start Pts a3"] = [0.05, 0.10, 0.15]  # complete
-CONFIG["Start Pts a4"] = [0.05, 0.15]  # complete
-CONFIG["Start Pts a5"] = [0.05, 0.15]  # complete
-CONFIG["Start Pts a6"] = [0.05]  # complete
-CONFIG["Start Pts a7"] = [0.05]  # complete
+CONFIG["Start Pts a0"] = [0.0, 0.05, 0.10, 0.15, 0.2]
+CONFIG["Start Pts a1"] = [0.0, 0.05, 0.10, 0.15, 0.2]
+CONFIG["Start Pts a2"] = [0.0, 0.05, 0.10, 0.15, 0.2]
+CONFIG["Start Pts a3"] = [0.05, 0.10, 0.15]
+CONFIG["Start Pts a4"] = [0.05, 0.15]
+CONFIG["Start Pts a5"] = [0.05, 0.15]
+CONFIG["Start Pts a6"] = [0.05]
+CONFIG["Start Pts a7"] = [0.05]
 # number of times to force bfgs to rerun for the initial fits
 CONFIG["Forced Extra Fits"] = 3
 # Number of refined points to use to generate initial positions for walkers
-CONFIG["Number Walker Generators"] = 20  # complete
+CONFIG["Number Walker Generators"] = 20
 # Spread of the initial sampling positions for walkers
-CONFIG["Sample Spread"] = 0.15  # complete
+CONFIG["Sample Spread"] = 0.15
+# Centroid of the offset applied to sampling positions for walkers
+CONFIG["Sample Offset Centroid"] = 0.02  # incomplete
+# Width of the offset applied to the sampling positions for walkers
+CONFIG["Sample Offset Width"] = 0.01  # incomplete
 # Number of points at the beginning of the Markov Chain to discard as 'burn in'
-CONFIG["Burn-in Points"] = 50  # complete
+# should be about one auto-correllation time
+CONFIG["Burn-in Points"] = 50
 # NOTES:
 # Note about how to calculate the number of sample points
 # the total number of sample points is the product of the following two values
