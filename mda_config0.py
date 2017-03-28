f6084e021f1ec4f146e8261d580c1f770bf46510
James Till Matta <jamesmatta@gmail.com>
2017-03-28 10:40:08 -0400

    Big reorganization of config file for clarity
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- mda_config0.py
+++ mda_config0.py
@@ -8,18 +8,42 @@ and is intended to be user editable"""
 # order agnostic
 CONFIG = {}
 
+###############################################################################
+#  MIMiC_MDA Calculation Library
+###############################################################################
 # path to the shared library that contains the compiled routines for fast calc
 # of chi^2, log likelihood, and prior distributions
 CONFIG["Shared Lib Path"] = "./C/libChiSq.so"
 
+###############################################################################
+#  Input Files and configuration (exp data and calculated distributions)
+###############################################################################
 # Information about inputs to the program
 # path to the file containing the experimental data
 CONFIG["Input File Path"] = "./58Ni_tz_fa_ce_mda.csv"
 # path to the directory that contains the dwba GR distributions
 # the distributions, regardless of sum rule fraction, should all start at 0 deg
-# and have identical sampling angles and end points
+# and have identical sampling angles and end points, the file names in the
+# folder must have the naming scheme:
+# "A{NucA:d}_Ex{ExEn:4.2f}_L{OrbAng:02d}_T{Iso:d}_F{Frac:03d}.csv"
+# where:
+# NucA is the nucleus's total number of nucleons
+# ExEn is the excitation energy which is always written with 2 decimal places
+# OrbAng is the two digit orbital angular momentum, padded with a '0' if needed
+# Iso is the isospin, which should always be zero except for the L01_T1 IVGDR
+# Frac is the sum rule fraction multiplied by 100 and converted to an integer
 CONFIG["Distribution Directory"] = "./58Ni_dist/"
+# A of the target nucleus, for reading input and writing output
+CONFIG["Target A"] = 58
+# List of sum rule fractions (on the interval [0, 1]), this can be used if you
+# want to perform the fit with varying sum rule fractions for each distribution
+# essentially, this affects the parameter limits and it affects which
+# distributions are read in for each L value
+CONFIG["EWSR Fractions"] = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
 
+###############################################################################
+#  Output Directory Structure
+###############################################################################
 # Information about outputs from the program
 # directory to hold the fitted parameters, also holds the diagnostic file
 CONFIG["Parameter Files Directory"] = "./fits1/"
@@ -39,6 +63,96 @@ CONFIG["Chain Directory"] = "./fits1/chains/"
 CONFIG["Parameter Plots Directory"] = "./fits1/param_plots/"
 # directory to store the autocorrellation plot sub directories
 CONFIG["Time Series Directory"] = "./fits1/walker_plots/"
+
+###############################################################################
+#  IVGDR Settings and information
+###############################################################################
+# Sets if IVGDR subtraction is carried carried out.
+CONFIG["Subtract IVGDR"] = True
+# Integral across all ex energy of the IVGDR lorientzian, doesn't matter if
+# subtraction is off
+CONFIG["IVGDR CS Integral"] = 294.0
+# Height of the IVGDR lorentzian in millibarns
+CONFIG["IVGDR Height"] = 23.6484
+# centroid energy (in MeV) of the IVGDR lorentzian
+CONFIG["IVGDR Center"] = 19.0984
+# width of the IVGDR lorentzian in MeV
+CONFIG["IVGDR Width"] = 7.91453
+
+###############################################################################
+#  Exp data limits and parameter limits
+###############################################################################
+# Information setting what energies and angles are fit
+# Maximum Angle (in degrees) to fit with
+CONFIG["Max Theta"] = 10.0
+# Energy such that all fitted energies are >= to it
+CONFIG["Start Energy"] = 8.4
+# Energy such that all fitted energies are <= to it
+CONFIG["Final Energy"] = 9.6  # 31.6
+# Limits on and paramters of the fit
+# Maximum L value to fit with
+CONFIG["Maximum L"] = 7
+
+###############################################################################
+#  Sampling and Concurrency Configuration
+###############################################################################
+# holds the number of points to sample for each walker, should be about 10
+# autocorrellation times, 1 auto correllation time is the time it takes for the
+# walkers for all parameters to drift into their designated sampling region
+CONFIG["Sample Points"] = 8193
+# holds the number of walks to run
+CONFIG["Number of Walkers"] = 2048
+# Number of points at the beginning of the Markov Chain to discard as 'burn in'
+# should be one to two of the longest auto-correlation times
+CONFIG["Burn-in Points"] = 512
+# holds the number of concurrent threads
+CONFIG["Number of Threads"] = 1
+
+
+###############################################################################
+#  Initial Fits Configuration
+###############################################################################
+# Lists of starting points for each a_L in the initial fits
+# (max a_L = 1.0/(EWSR Fraction_L)
+# ... This continues for as many Ls as you have
+# for example: CONFIG["Start Pts a23"] = [0.5]
+CONFIG["Start Pts a0"] = [0.0, 0.05, 0.10, 0.15, 0.2]
+CONFIG["Start Pts a1"] = [0.0, 0.05, 0.10, 0.15, 0.2]
+CONFIG["Start Pts a2"] = [0.0, 0.05, 0.10, 0.15, 0.2]
+CONFIG["Start Pts a3"] = [0.05, 0.10, 0.15]
+CONFIG["Start Pts a4"] = [0.05, 0.15]
+CONFIG["Start Pts a5"] = [0.05, 0.15]
+CONFIG["Start Pts a6"] = [0.05, 0.15]
+CONFIG["Start Pts a7"] = [0.05, 0.15]
+# Spread of the initial sampling positions for walkers
+CONFIG["Sample Spread"] = 2.0
+# Centroid of the offset applied to sampling positions for walkers
+CONFIG["Sample Offset Centroid"] = 0.04
+# Width of the offset applied to the sampling positions for walkers
+CONFIG["Sample Offset Width"] = 0.02
+# number of times to force bfgs to rerun for the initial fits
+CONFIG["Forced Extra Fits"] = 3
+# Number of refined points to use to generate initial positions for walkers
+CONFIG["Number Walker Generators"] = 50
+
+###############################################################################
+#  Parameter and Error Finding configuration
+###############################################################################
+# Confidence interval of the error bars
+# number of sample points (see notes below) must exceed, by a fair margin,
+# the following value: 1 / ((1 - CONFIG["Confidence Interval"]) / 2)
+# 1 sigma -> 0.682689492
+# 2 sigma -> 0.954499736
+# 3 sigma -> 0.997300204
+# 4 sigma -> 0.99993666
+CONFIG["Confidence Interval"] = 0.682689492
+# holds the number of bins to use when determining the peak of the probability
+# density distribution
+CONFIG["Num Bins"] = 1000
+
+###############################################################################
+#  Plot Configuration
+###############################################################################
 # format for plot outputs. options are:
 # "svg" - scalable vector graphics
 # "svgz" - scalable vector graphics (compressed)
@@ -64,72 +178,24 @@ CONFIG["Generate Corner Plots"] = True
 CONFIG["Corner Plot Samples"] = 1000000
 # sets the number of bins for each parameter axis in the corner plot
 CONFIG["Corner Plot Bins"] = [100, 100, 100, 100, 100, 100, 100, 100]
-# allow the corner package to set param ranges instead of [0, param_max]
+# allow the corner package to set param ranges instead of [0, 1.1*param_max]
 CONFIG["Corner Default Range"] = True
 # sets whether or not the user wants to plot the IVGDR in the unlimited fit
 # plots
 CONFIG["Plot IVGDR in Fits"] = False
 # sets the max L the user wants to plot in the limited fit plot
 CONFIG["Fit Plot L Limit"] = 3
+# Number of walkers to plot in the time series plots
+CONFIG["Walker Plot Count"] = 1000
+
+
+###############################################################################
+#  Output/Diagnostic Generation Options
+###############################################################################
 # Toggle the output of sample chains, if you disable the output of Corner plots
 # this needs to be on if you later want to make the corner plots in the
 # seperate script if you want to analyze the samples in other ways
 CONFIG["Save Chain Data"] = False
-# Number of walkers to plot in the time series plots
-CONFIG["Walker Plot Count"] = 1000
-
-# Information about the target nucleus
-# A of the target nucleus
-CONFIG["Target A"] = 58
-# Sets if IVGDR subtraction is carried carried out.
-CONFIG["Subtract IVGDR"] = True
-# Integral across all ex energy of the IVGDR lorientzian, doesn't matter if
-# subtraction is off
-CONFIG["IVGDR CS Integral"] = 294.0
-# Height of the IVGDR lorentzian in millibarns
-CONFIG["IVGDR Height"] = 23.6484
-# centroid energy (in MeV) of the IVGDR lorentzian
-CONFIG["IVGDR Center"] = 19.0984
-# width of the IVGDR lorentzian in MeV
-CONFIG["IVGDR Width"] = 7.91453
-
-# Information setting what energies and angles are fit
-# Maximum Angle (in degrees) to fit with
-CONFIG["Max Theta"] = 10.0
-# Energy such that all fitted energies are >= to it
-CONFIG["Start Energy"] = 8.4
-# Energy such that all fitted energies are <= to it
-CONFIG["Final Energy"] = 31.6
-
-# Limits on and paramters of the fit
-# Maximum L value to fit with
-CONFIG["Maximum L"] = 7
-# List of sum rule fractions (on the interval [0, 1])
-CONFIG["EWSR Fractions"] = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-
-# Confidence interval of the error bars
-# number of sample points (see notes below) must exceed, by a fair margin,
-# the following value: 1 / ((1 - CONFIG["Confidence Interval"]) / 2)
-# 1 sigma -> 0.682689492
-# 2 sigma -> 0.954499736
-# 3 sigma -> 0.997300204
-# 4 sigma -> 0.99993666
-CONFIG["Confidence Interval"] = 0.682689492
-# holds the number of points to sample for each walker, should be about 10
-# autocorrellation times, 1 auto correllation time is the time it takes for the
-# walkers for all parameters to drift into their designated sampling region
-CONFIG["Sample Points"] = 16385
-# holds the number of walks to run
-CONFIG["Number of Walkers"] = 2048
-# holds the number of concurrent threads
-CONFIG["Number of Threads"] = 2
-# holds the number of bins to use when determining the peak of the probability
-# density distribution
-CONFIG["Num Bins"] = 1000
-# holds the difference required to make floating point comparisons evaluate
-# as equal values
-CONFIG["Float Epsilon"] = 1.0e-7
-
 # Autocorrelation check parameters
 # holds if the autocorrelation time should be calculated, its nice to know,
 # but requires a lot of extra samples to know accurately
@@ -137,51 +203,15 @@ CONFIG["Calc AutoCorr"] = True
 # holds the number of autocorrellation times required to estimate the
 # autocorrelation time of the chains the larger this is the more accurate the
 # estimate
-# NOTES: a word of warning on this parameter, the autocorrellation time check
-# will fail under the following circumstances:
-# AC_WIND = CONFIG["ACorr WindSize"]
-# CONFIG["Sample Points"]/AC_WIND < (AC_WIND * Longest Autocorrelation Time)
-# it is best to do tests with single energies, or set the number of sample
-# points to a very large number to be certain
-# to state the condition above another way:
-# AC_WIND*AC_WIND > CONFIG["Sample Points"]/Longest Autocorrelation Time
-CONFIG["ACorr WindSize"] = 5.0
+# NOTES: a word of warning on autocorrelation checks: they require either a
+# very short window size (CONFIG["ACorr WindSize"] or AC_WIND) which can reduce
+# the accurace of the autocorrellation time calculation, or they require a lot
+# more than the recommended number of samples. In particular you need to
+# fulfill 2*AC_WIND*AC_WIND*(Longest ACorr Time) < CONFIG["Sample Points"]
+CONFIG["ACorr WindSize"] = 3.5
 # This parameter sets if the effective sample size for the autocorrelation time
 # calculation should be restricted to 2^n samples (where n is as large as,
 # possible such that 2^n <= CONFIG["Sample Points"]), this does significantly
 # speed up acorr time calculation, but it does shrink the effective number of
 # pointes available for calculating the acorr time
 CONFIG["ACorr Use FFT"] = True
-
-# Lists of starting points for each a_L in the initial fits
-# (max a_L = 1.0/(EWSR Fraction_L)
-# ... This continues for as many Ls as you have
-# for example: CONFIG["Start Pts a23"] = [0.5]
-CONFIG["Start Pts a0"] = [0.0, 0.05, 0.10, 0.15, 0.2]
-CONFIG["Start Pts a1"] = [0.0, 0.05, 0.10, 0.15, 0.2]
-CONFIG["Start Pts a2"] = [0.0, 0.05, 0.10, 0.15, 0.2]
-CONFIG["Start Pts a3"] = [0.05, 0.10, 0.15]
-CONFIG["Start Pts a4"] = [0.05, 0.15]
-CONFIG["Start Pts a5"] = [0.05, 0.15]
-CONFIG["Start Pts a6"] = [0.05, 0.15]
-CONFIG["Start Pts a7"] = [0.05, 0.15]
-# number of times to force bfgs to rerun for the initial fits
-CONFIG["Forced Extra Fits"] = 3
-# Number of refined points to use to generate initial positions for walkers
-CONFIG["Number Walker Generators"] = 50
-# Spread of the initial sampling positions for walkers
-CONFIG["Sample Spread"] = 0.75
-# Centroid of the offset applied to sampling positions for walkers
-CONFIG["Sample Offset Centroid"] = 0.02
-# Width of the offset applied to the sampling positions for walkers
-CONFIG["Sample Offset Width"] = 0.01
-# Number of points at the beginning of the Markov Chain to discard as 'burn in'
-# should be about one auto-correllation time
-CONFIG["Burn-in Points"] = 300
-# NOTES:
-# Note about how to calculate the number of sample points
-# the total number of sample points is the product of the following two values
-# number of useable sample points per walker:
-# (CONFIG["Sample Points"]-CONFIG["Burn-in Points"])
-# and the number of walkers:
-# CONFIG["Number of Walkers"])
